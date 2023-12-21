9d0d821fe4b4bea90e10bfc46e7c613470ee2c58
wtf403 <wtf403@yandex.ru>
2023-12-21 04:42:01 +0300

    fix layout

--- client/src/pages/WorkroomScheme.vue
+++ client/src/pages/WorkroomScheme.vue
@@ -1,36 +1,82 @@
 <template>
   <UIHeader title="Планировка цехов" />
   <UINav />
-  <div class="workroom__head">
-    <UISelect v-model="selectedWorkroom" placeholder="Планы цехов" class="status-filter">
-      <option value="design">Цех оформления</option>
-      <option value="mounting">Цех монтажа тортов</option>
-      <option value="packing">Упаковочный цех</option>
-      <option value="bakery">Пекарный цех</option>
-      <option value="billet">Заготовительный цех</option>
-    </UISelect>
-    <UIButton @click="deleteAll">Удалить все</UIButton>
-    <UIButton>Сохранить</UIButton>
-  </div>
-  <div v-if="selectedWorkroom" @dragover.prevent @drop="dropBadge" class="workroom__wrapper">
-    <div v-for="badge in this.selectedBadges" :key="badge.name" class="scheme__badge"
-      :style="{ top: `${Math.abs(rotation%180)==90?badge.x:badge.y}px`, left: `${rotation%180==90?badge.y:badge.x}px` }" draggable @dragstart="dragStart(badge, true)">
-      <UIButton class="badge__button-close" @click="deleteBadge(badge.id)">
-        <img class="badge__close" src="../assets/cross-icon.png" alt="Delete this badge">
-      </UIButton>
-      <img class="badge__image" :src="getBadgeImage(badge.name)" alt="Badge">
+  <div class="page">
+    <div class="workroom__head">
+      <UISelect
+        v-model="selectedWorkroom"
+        placeholder="Планы цехов"
+        class="status-filter"
+      >
+        <option value="design">Цех оформления</option>
+        <option value="mounting">Цех монтажа тортов</option>
+        <option value="packing">Упаковочный цех</option>
+        <option value="bakery">Пекарный цех</option>
+        <option value="billet">Заготовительный цех</option>
+      </UISelect>
+      <UIButton @click="deleteAll">Удалить все</UIButton>
+      <UIButton>Сохранить</UIButton>
     </div>
-    <img class="scheme__image" ondragstart="return false;" ondrop="return false;" :src="selectedImage" v-if="selectedImage" alt="Selected workroom"
-      :style="{ transform: `rotate(${rotation}deg)`, margin: rotation%180==0?`0`:`140px 0` }">
-  </div>
-  <div v-if="selectedWorkroom" class="badges__wrapper">
-    <div v-for="badge in badges" :key="badge.name" class="badge" :draggable="true" @dragstart="dragStart(badge, false)">
-      <img class="badge__image" :src="badge.image" :alt="badge.name">
+    <div>
+      <div
+        v-if="selectedWorkroom"
+        @dragover.prevent
+        @drop="dropBadge"
+        class="workroom__wrapper"
+      >
+        <div
+          v-for="badge in this.selectedBadges"
+          :key="badge.name"
+          class="scheme__badge"
+          :style="{
+            top: `${Math.abs(rotation % 180) == 90 ? badge.x : badge.y}px`,
+            left: `${Math.abs(rotation % 180) == 90 ? badge.y : badge.x}px`,
+          }"
+          draggable
+          @dragstart="dragStart(badge, true)"
+        >
+          <UIButton class="badge__button-close" @click="deleteBadge(badge.id)">
+            <img
+              class="badge__close"
+              src="../assets/cross-icon.png"
+              alt="Delete this badge"
+            />
+          </UIButton>
+          <img
+            class="badge__image"
+            :src="getBadgeImage(badge.name)"
+            alt="Badge"
+          />
+        </div>
+        <img
+          class="scheme__image"
+          ondragstart="return false;"
+          ondrop="return false;"
+          :src="selectedImage"
+          v-if="selectedImage"
+          alt="Selected workroom"
+          :style="{
+            transform: `rotate(${rotation}deg)`,
+            margin: rotation % 180 == 0 ? `0` : `140px 0`,
+          }"
+        />
+      </div>
+      <div v-if="selectedWorkroom" class="badges__wrapper">
+        <div
+          v-for="badge in badges"
+          :key="badge.name"
+          class="badge"
+          :draggable="true"
+          @dragstart="dragStart(badge, false)"
+        >
+          <img class="badge__image" :src="badge.image" :alt="badge.name" />
+        </div>
+      </div>
     </div>
-  </div>
-  <div v-if="selectedWorkroom" class="scheme__actions">
-    <UIButton @click="doRatate(-90)">Влево</UIButton>
-    <UIButton @click="doRatate(90)">Вправо</UIButton>
+    <div v-if="selectedWorkroom" class="scheme__actions">
+      <UIButton @click="doRotate(-90)">Влево</UIButton>
+      <UIButton @click="doRotate(90)">Вправо</UIButton>
+    </div>
   </div>
 </template>
 
@@ -51,14 +97,12 @@ import FireExtinguisher from "@/assets/badges/FireExtinguisher.png";
 import Exit from "@/assets/badges/Exit.jpg";
 import Equipment from "@/assets/badges/Equipment.png";
 
-
-
 export default {
   components: {
     UIHeader,
     UINav,
     UISelect,
-    UIButton
+    UIButton,
   },
   data() {
     return {
@@ -67,23 +111,23 @@ export default {
       rotation: 0,
       badges: [
         {
-          name: 'FirstAid',
+          name: "FirstAid",
           image: FirstAid,
         },
         {
-          name: 'FireExtinguisher',
+          name: "FireExtinguisher",
           image: FireExtinguisher,
         },
         {
-          name: 'Exit',
+          name: "Exit",
           image: Exit,
         },
         {
-          name: 'Equipment',
+          name: "Equipment",
           image: Equipment,
-        }
+        },
       ],
-      workrooms: [
+      workrooms: JSON.parse(localStorage.getItem("workrooms")) || [
         {
           name: "design",
           title: "Цех оформления",
@@ -94,110 +138,129 @@ export default {
               id: 1,
               name: "FirstAid",
               x: 50,
-              y: 50
+              y: 50,
             },
-          ]
+          ],
         },
         {
           name: "mounting",
           title: "Цех монтажа тортов",
           image: mounting,
           rotation: 0,
-          badges: []
+          badges: [],
         },
         {
           name: "packing",
           title: "Упаковочный цех",
           image: packing,
           rotation: 0,
-          badges: []
+          badges: [],
         },
         {
           name: "bakery",
           title: "Пекарный цех",
           image: bakery,
           rotation: 0,
-          badges: []
+          badges: [],
         },
         {
           name: "billet",
           title: "Заготовительный цех",
           image: billet,
           rotation: 0,
-          badges: []
+          badges: [],
         },
       ],
     };
   },
   computed: {
     selectedRotation() {
-      const room = this.workrooms.find((room) => room.name === this.selectedWorkroom);
+      const room = this.workrooms.find(
+        (room) => room.name === this.selectedWorkroom
+      );
       return room ? room.rotation : 0;
     },
     selectedImage() {
-      const room = this.workrooms.find((room) => room.name === this.selectedWorkroom);
+      const room = this.workrooms.find(
+        (room) => room.name === this.selectedWorkroom
+      );
       return room ? room.image : null;
     },
-    selectedBadges(){
+    selectedBadges() {
       if (this.selectedWorkroom) {
-          return this.workrooms.find(room => room.name === this.selectedWorkroom).badges
-        }
-        return [];
+        return this.workrooms.find(
+          (room) => room.name === this.selectedWorkroom
+        ).badges;
+      }
+      return [];
     },
   },
   methods: {
     dragStart(badge, deleteValue) {
       badge.delete = deleteValue;
-      console.log(badge)
-      event.dataTransfer.setData('text/plain', JSON.stringify(badge));
+      console.log(badge);
+      event.dataTransfer.setData("text/plain", JSON.stringify(badge));
     },
     dropBadge(event) {
       event.preventDefault();
-      const badgeData = JSON.parse(event.dataTransfer.getData('text/plain'));
+      const badgeData = JSON.parse(event.dataTransfer.getData("text/plain"));
       const rect = event.currentTarget.getBoundingClientRect();
       const mouseX = event.clientX;
       const mouseY = event.clientY;
+
       const x = mouseX - rect.left;
       const y = mouseY - rect.top;
-      const X = Math.abs(this.rotation%180) == 90 ? y - 20 : x - 20; 
-      const Y = Math.abs(this.rotation%180) == 90 ? x - 20 : y - 20;
-      const selectedRoom = this.workrooms.findIndex((room) => room.name === this.selectedWorkroom);
+      const X = Math.abs(this.rotation % 180) == 90 ? y - 20 : x - 20;
+      const Y = Math.abs(this.rotation % 180) == 90 ? x - 20 : y - 20;
+      const selectedRoom = this.workrooms.findIndex(
+        (room) => room.name === this.selectedWorkroom
+      );
       const obj = {
         id: this.allID,
         name: badgeData.name,
         x: X,
         y: Y,
-      }
+      };
       this.allID += 1;
       this.workrooms[selectedRoom].badges.push(obj);
-      console.log(this.selectedBadges)
-      if (badgeData.delete){
-        this.deleteBadge(badgeData.id)
+      console.log(this.selectedBadges);
+      if (badgeData.delete) {
+        this.deleteBadge(badgeData.id);
       }
+
+      localStorage.setItem("workrooms", JSON.stringify(this.workrooms));
     },
     getBadgeImage(name) {
       const badge = this.badges.find((badge) => badge.name === name);
-      return badge ? badge.image : '';
+      return badge ? badge.image : "";
     },
-    setBadges(newValue){
-        const selectedRoom = this.workrooms.findIndex(room => room.name === this.selectedWorkroom)
-        this.workrooms[selectedRoom].badges = newValue
+    setBadges(newValue) {
+      const selectedRoom = this.workrooms.findIndex(
+        (room) => room.name === this.selectedWorkroom
+      );
+      this.workrooms[selectedRoom].badges = newValue;
     },
-    deleteAll(){
-      const selectedRoom = this.workrooms.findIndex(room => room.name === this.selectedWorkroom)
-      this.workrooms[selectedRoom].badges = []
+    deleteAll() {
+      const selectedRoom = this.workrooms.findIndex(
+        (room) => room.name === this.selectedWorkroom
+      );
+      this.workrooms[selectedRoom].badges = [];
+      localStorage.setItem("workrooms", JSON.stringify(this.workrooms));
     },
-    deleteBadge(id){
-      console.log(id)
-      const selectedRoom = this.workrooms.findIndex(room => room.name === this.selectedWorkroom)
-      this.workrooms[selectedRoom].badges = this.workrooms[selectedRoom].badges.filter(item => item.id !== id);
+    deleteBadge(id) {
+      console.log(id);
+      const selectedRoom = this.workrooms.findIndex(
+        (room) => room.name === this.selectedWorkroom
+      );
+      this.workrooms[selectedRoom].badges = this.workrooms[
+        selectedRoom
+      ].badges.filter((item) => item.id !== id);
     },
-    doRatate(val){
+    doRotate(val) {
       this.rotation += val;
-    }
+    },
   },
 };
-
 </script>
 
 <style scoped>
@@ -211,19 +274,33 @@ export default {
   gap: 1rem;
 }
 
+.page {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+}
 .workroom__wrapper {
   transform-origin: center;
   position: relative;
-  width: 600px;
-  height: 600px;
-  border: 1px dashed #ccc;
+  margin: 0 auto;
   display: flex;
+  max-height: 50rem;
   flex-wrap: wrap;
   padding: 10px;
   align-content: center;
 }
 
+.scheme__image {
+  width: 500px;
+  height: 500px;
+  object-fit: contain;
+}
+
 .badges__wrapper {
+  justify-content: center;
   display: flex;
   align-items: center;
   gap: 10px;
@@ -243,6 +320,10 @@ export default {
   z-index: 4;
 }
 
+.status-filter {
+  margin-right: auto;
+}
+
 .badge__image {
   width: 40px;
   height: 40px;
@@ -250,7 +331,10 @@ export default {
 }
 .workroom__head {
   display: flex;
+  width: 100%;
   gap: 10px;
+  margin-top: 2rem;
+  padding-inline: 2rem;
   flex-wrap: wrap;
 }
 
@@ -267,7 +351,7 @@ export default {
   display: none;
 }
 
-.scheme__badge:hover .badge__button-close{
+.scheme__badge:hover .badge__button-close {
   display: block;
 }
 
@@ -275,5 +359,6 @@ export default {
   display: flex;
   gap: 10px;
   flex-wrap: wrap;
+  margin-bottom: 2rem;
 }
 </style>
